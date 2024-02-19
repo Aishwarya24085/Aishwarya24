1bac771d269562cd8dada73c832960e0fea67016
Aishwarya24085 <158800396+Aishwarya24085@users.noreply.github.com>
2024-02-19 20:56:28 +0530

    Headerfile.cpp

--- Headerfile.cpp
+++ Headerfile.cpp
@@ -9,6 +9,16 @@ int main()
     float l,w,h;
     cout<<"Enter length,width and height of the box:";
     cin>>l>>w>>h;
-    boxArea(l,w,h);
-    boxVolume(l,w,h);
+    #ifdef AREA
+        boxArea(l,w,h);
+    #endif
+    #ifndef AREA
+        cout<<"Box area cannot be found!! header file is not included\n";
+    #endif
+    #ifdef VOLUME
+        boxVolume(l,w,h);
+    #endif
+    #ifndef VOLUME
+        cout<<"Box volume cannot be found!! header file is not included\n";
+    #endif
 }
