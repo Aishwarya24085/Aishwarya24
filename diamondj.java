e82ca5a3edfe74c571027ab695e7d3876bacda54
Aishwarya24085 <158800396+Aishwarya24085@users.noreply.github.com>
2024-03-06 21:55:12 +0530

    diamondj.java

--- diamondj.java
+++ diamondj.java
@@ -3,7 +3,7 @@ class ClassA1
     public
     void aClass()
     {
-        System.out.print("Iam a ClassA"+"\n");
+        System.out.println("Iam a ClassA");
     }
 }
 class ClassB1 extends ClassA1
@@ -11,7 +11,7 @@ class ClassB1 extends ClassA1
     public
     void bClass()
     {
-        System.out.print("Iam a ClassB"+"\n");
+        System.out.println("Iam a ClassB");
     }
 }
 class ClassC1 extends ClassA1
@@ -19,19 +19,21 @@ class ClassC1 extends ClassA1
     public
     void cClass()
     {
-        System.out.print("Iam a ClassC"+"\n");
+        System.out.println("Iam a ClassC");
     }
 }
-class ClassD extends ClassA,ClassB
+/*
+class ClassD extends ClassA1,ClassB1
 {
 }
+*/
 //Here the actual problem occurs where the support of Multiple Inheritance in java is
 //eliminated.
 public class diamondj
 {
     public static void main(String[] args)
     {
-    System.out.print("Diamond Problem"+"\n");
+    System.out.println("Diamond Problem");
     ClassB1 class1=new ClassB1();
     ClassC1 class2=new ClassC1();
     class1.aClass();
